c627305ff75ad62d274b5f303e06fd47e39117b3
Derek Howard <derekhoward@gmail.com>
2016-11-29 16:33:46 -0500

    Update site and remove unused bits

--- My Democracy_files/application.js
+++ My Democracy_files/application.js
@@ -1,17 +1,23 @@
 var localeObj = {
-  appName: "MyDemocracy.ca",
-  warningText: "will be coming soon"
+  logo: 'logo@3x.png',
+  warningText: "MyDemocracy.ca will be available soon. To be notified, enter your email below:",
+  placeholder: "ex. email@example.com",
+  submit: "SUBMIT"
 };
 
 if(window.location.hostname == "www.mademocratie.ca") {
   localeObj = {
-    appName: "Madémocratie.ca",
-    warningText: "sera bientôt lancée"
+    logo: 'logo-f-r@3x.png',
+    warningText: "MaDemocratie.ca sera bientôt lancée. Si vous aimeriez qu’on vous prévienne lorsque le site sera disponible, inscrivez votre adresse courriel ci-dessous.",
+    placeholder: "ex. adresse@exemple.com",
+    submit: "ENVOYER"
   };
 }
 
-document.querySelectorAll('.warning-app-name')[0].innerText = localeObj.appName;
 document.querySelectorAll('.warning-text')[0].innerText = localeObj.warningText;
+document.querySelectorAll('.warning-logo')[0].src = localeObj.logo;
+document.querySelectorAll('.form-text')[0].placeholder = localeObj.placeholder;
+document.querySelectorAll('.form-submit')[0].value = localeObj.submit;
 
 el = document.querySelectorAll('.warning-holder')[0];
 var className = 'visible';
